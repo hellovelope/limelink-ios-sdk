45a30b5a53576ec3cd490c17820643f3011605e0
hysong <hysong@artue.io>
2024-06-21 15:53:05 +0900

    fix: update

--- Example/LimelinkIOSSDK/ViewController.swift
+++ Example/LimelinkIOSSDK/ViewController.swift
@@ -11,7 +11,7 @@ import LimelinkIOSSDK
 
 public class ViewController: UIViewController {
 
-    public override func viewDidLoad() {       
+    public override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
