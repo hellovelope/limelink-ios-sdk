4152b8de58a7b203ca4b8fd649e19916474f00ef
hysong <hysong@artue.io>
2024-06-24 12:03:36 +0900

    fix: update

---
+++ Classes/request/LimeLinkRequest.swift
@@ -0,0 +1,24 @@
+//
+//  LimeLinkRequest.swift
+//  LimelinkIOSSDK
+//
+//  Created by artue on 6/24/24.
+//
+
+import Foundation
+
+public class LimeLinkRequest: Codable {
+    var privateKey: String
+    var suffix: String
+    var handle: String?
+    var eventType: String
+    var operatingSystem: String
+
+    init(privateKey: String, suffix: String, handle: String? = nil, eventType: String, operatingSystem: String = "android") {
+        self.privateKey = privateKey
+        self.suffix = suffix
+        self.handle = handle
+        self.eventType = eventType
+        self.operatingSystem = operatingSystem
+    }
+}
