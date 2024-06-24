e36168ebdada19208172dd58cc0be20482dbbee0
hysong <hysong@artue.io>
2024-06-24 13:23:29 +0900

    fix: update file

---
+++ LimelinkIOSSDK/Classes/enum/EventType.swift
@@ -0,0 +1,13 @@
+//
+//  EventType.swift
+//  LimelinkIOSSDK
+//
+//  Created by artue on 6/24/24.
+//
+
+import Foundation
+
+enum EventType: String {
+    case FIRST_RUN = "first_run"
+    case RERUN = "rerurn"
+}
---
+++ LimelinkIOSSDK/Classes/response/PathParamResponse.swift
@@ -0,0 +1,18 @@
+//
+//  PathParamResponse.swift
+//  LimelinkIOSSDK
+//
+//  Created by artue on 6/24/24.
+//
+
+import Foundation
+
+public class PathParamResponse {
+    var mainPath: String
+    var subPath: String?
+    
+    init(mainPath: String, subPath: String? = nil) {
+        self.mainPath = mainPath
+        self.subPath = subPath
+    }
+}
